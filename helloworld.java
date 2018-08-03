c5b8acd08b37a89c184d93fee28772033bbbafd9
qatestid12 <42012669+qatestid12@users.noreply.github.com>
2018-08-03 14:35:03 +0500

    added test files helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,19 @@
+public class helloworld {
+
+	public static void main(String[] args) {
+
+		
+	System.out.println("MY First CICD Project");
+
+		for(int x = 1; x < 21; x = x + 1) {
+	
+	  
+	         System.out.println(x + " HelloWorld");
+
+		
+	         
+	      }
+
+	}
+
+}
